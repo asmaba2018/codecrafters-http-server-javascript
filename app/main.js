8446d1466250afe5b119d291b03a91db6576ec6e
Ahmed Sherif <eng_ahmed.sherif@yahoo.com>
2024-07-24 14:03:44 +0200

    Respond With userAgent, echo, 200 & 404

--- app/main.js
+++ app/main.js
@@ -4,32 +4,43 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
+const flags = process.argv.slice(2);
+const directory = flags.find((_, index) => flags[index - 1] == "--directory");
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const request = data.toString();
+    const [request, host, agent] = data.toString().split("\r\n");
 
-    const url = request.split(" ")[1];
-    console.log(url);
+    const [method, path, version] = request.split(" ");
 
-    const headers = request.split("\r\n");
-    console.log(headers);
-
-    if (url == "/") {
-	const httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-	socket.write(httpResponse);
-    } else if (url.includes("/echo/")) {
-	const content = request.split("/echo/")[1].split(" ")[0];
-	// console.log(content);
-	const httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
-	socket.write(httpResponse);
-    } else if (url == ("/user-agent")) {
-	const userAgent = headers[2].split("User-Agent: ")[1];
-	// console.log(userAgent);
-	const httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-	socket.write(httpResponse);
-    } else {
-	const httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-	socket.write(httpResponse);
+    if (method == "GET") {
+	if (path == "/") {
+	  const httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+	  socket.write(httpResponse);
+	} else if (path.startsWith("/echo/")) {
+	  const content = path.split("/echo/")[1];
+	  // console.log(content);
+	  const httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+	  socket.write(httpResponse);
+	} else if (path == ("/user-agent")) {
+	  const userAgent = agent.split("User-Agent: ")[1];
+	  // console.log(userAgent);
+	  const httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+	  socket.write(httpResponse);
+	} else if (path.startsWith("/files/")){
+	  const filePath = path.split("/files/")[1];
+	  if(!fs.existsSync(directory + filePath)){
+	    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+	    socket.end();
+	  } else {
+		const file = fs.readFileSync(directory + filePath);
+		const httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`
+		socket.write(httpResponse);
+            }
+	  }
+	} else {
+	  const httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+	  socket.write(httpResponse);
     }
 
     socket.end();
